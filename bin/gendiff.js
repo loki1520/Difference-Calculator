d2ea9314986319bd305bf399d49ee11759ba1a74
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-08-25 09:22:49 +0530

    Prepare project

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
+console.log('Hello, project');
