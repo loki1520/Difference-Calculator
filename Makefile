6f63f348d19a70b532da2bfebd156a00227d339a
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-08-25 18:26:31 +0530

    First stage

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-install:
+install-deps:
 	npm ci
 
 gendiff:
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,9 @@ const program = new Command();
 
 program
   .name('gendiff')
-  .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse();
-console.log('Hello, project');
+// console.log(program);
