d72ed398a138517d3b58dc52f212aaf2ebc2a73c
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-08-29 21:29:41 +0530

    3/4_Step3

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import fs from 'fs';
 import path from 'path';
 import comparison from './comparison.js';
 
-const genDiff = (filepath1, filepath2) => {
-  const absolutePathToFile1 = path.resolve(process.cwd(), filepath1);
-  const absolutePathToFile2 = path.resolve(process.cwd(), filepath2);
+const getCurrentWorkDir = () => process.cwd();
+const getAbsolutePathFile = (filepath) => path.resolve(getCurrentWorkDir(), filepath);
+const getObj = (data) => JSON.parse(fs.readFileSync(data, 'utf-8'));
+const getFileExtention = (filepath) => filepath.slice(filepath.lastIndexOf('.') + 1);
 
-  const obj1 = JSON.parse(fs.readFileSync(absolutePathToFile1, 'utf-8'));
-  const obj2 = JSON.parse(fs.readFileSync(absolutePathToFile2, 'utf-8'));
+const genDiff = (filepath1, filepath2) => {
+  const absolutePathToFile1 = getAbsolutePathFile(filepath1);
+  const absolutePathToFile2 = getAbsolutePathFile(filepath2);
+
+  const obj1 = getObj(absolutePathToFile1);
+  const obj2 = getObj(absolutePathToFile2);
 
   return comparison(obj1, obj2);
 };
