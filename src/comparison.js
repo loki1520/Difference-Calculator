94c9e8b75f275af2c8503bfcf443c6e874ee447e
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-09-04 10:44:17 +0530

    Add tree

--- src/comparison.js
+++ src/comparison.js
@@ -1,22 +1,57 @@
 import _ from 'lodash';
 
 const comparison = (obj1, obj2) => {
-  const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
+  const keys = _.sortBy(_.uniq(Object.keys(obj1), Object.keys(obj2)));
 
-  let result = '';
-
-  keys.map((key) => {
-    if (_.has(obj1, key) && _.has(obj2, key) && obj1[key] === obj2[key]) {
-      result += `  ${key}: ${obj1[key]}\n`;
-    } else if (_.has(obj1, key) && _.has(obj2, key) && obj1[key] !== obj2[key]) {
-      result += `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}\n`;
-    } else if (_.has(obj1, key)) {
-      result += `  - ${key}: ${obj1[key]}\n`;
-    } else if (_.has(obj2, key)) {
-      result += `  + ${key}: ${obj2[key]}\n`;
+  const getTree = keys.map((key) => {
+    if (key in obj1 && key in obj2 && obj1[key] === obj2[key]) {
+      return {
+        key,
+        value: obj1[key],
+        type: 'unchanged',
+      };
     }
-    return result;
+    if (key in obj1 && key in obj2 && obj1[key] !== obj2[key]) {
+      return {
+        key,
+        value1: obj1[key],
+        value2: obj2[key],
+        type: 'changed',
+      };
+    }
+    if (!(key in obj2)) {
+      return {
+        key,
+        value: obj1[key],
+        type: 'deleted',
+      };
+    }
+    if (!(key in obj1)) {
+      return {
+        key,
+        value: obj2[key],
+        type: 'added',
+      };
+    }
+    return getTree;
   });
+
+  const result = getTree.reduce((acc, obj) => {
+    if (obj.type === 'unchanged') {
+      acc += `  ${obj.key}: ${obj.value}\n`;
+    }
+    if (obj.type === 'changed') {
+      acc += `  - ${obj.key}: ${obj.value1}\n  + ${obj.key}: ${obj.value2}\n`;
+    }
+    if (obj.type === 'deleted') {
+      acc += `  - ${obj.key}: ${obj.value}\n`;
+    }
+    if (obj.type === 'added') {
+      acc += `  + ${obj.key}: ${obj.value}\n`;
+    }
+    return acc;
+  }, '');
+
   return `{\n${result}}`;
 };
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import comparison from './comparison.js';
 const getCurrentWorkDir = () => process.cwd();
 const getAbsolutePathFile = (filepath) => path.resolve(getCurrentWorkDir(), filepath);
 const getObj = (data) => JSON.parse(fs.readFileSync(data, 'utf-8'));
-const getFileExtention = (filepath) => filepath.slice(filepath.lastIndexOf('.') + 1);
+// const getFileExtention = (filepath) => filepath.slice(filepath.lastIndexOf('.') + 1);
 
 const genDiff = (filepath1, filepath2) => {
   const absolutePathToFile1 = getAbsolutePathFile(filepath1);
