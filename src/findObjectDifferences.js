b95a945b8709da99dee81da38b4437afa4a330c2
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-09-05 13:40:08 +0530

    Fix findObjectDifferences

--- src/findObjectDifferences.js
+++ src/findObjectDifferences.js
@@ -5,16 +5,16 @@ const findObjectDifferences = (obj1, obj2) => {
 
   const result = prepairedTree.reduce((acc, obj) => {
     if (obj.type === 'unchanged') {
-      acc += `  ${obj.key}: ${obj.value}\n`;
+      return `${acc}  ${obj.key}: ${obj.value}\n`;
     }
     if (obj.type === 'changed') {
-      acc += `  - ${obj.key}: ${obj.value1}\n  + ${obj.key}: ${obj.value2}\n`;
+      return `${acc}  - ${obj.key}: ${obj.value1}\n  + ${obj.key}: ${obj.value2}\n`;
     }
     if (obj.type === 'deleted') {
-      acc += `  - ${obj.key}: ${obj.value}\n`;
+      return `${acc}  - ${obj.key}: ${obj.value}\n`;
     }
     if (obj.type === 'added') {
-      acc += `  + ${obj.key}: ${obj.value}\n`;
+      return `${acc}  + ${obj.key}: ${obj.value}\n`;
     }
     return acc;
   }, '');
