36695136c2177c05b949f019e0113247db585cf6
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-09-09 23:02:01 +0530

    Fix tests

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -7,7 +7,7 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, './', '__fixtures__', filename);
 const renderResult = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
