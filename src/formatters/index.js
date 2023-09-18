f6357d91d0fda23fd3e07de1cd4ce2484b936a06
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-09-18 19:54:00 +0530

    Fix Step8

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -11,14 +11,3 @@ const typeConclusion = {
 const resultConclusion = (obj1, obj2, format = 'stylish') => typeConclusion[format](obj1, obj2);
 
 export default resultConclusion;
-
-// const resultConclusion = (obj1, obj2, format) => {
-//   switch (format) {
-//     case 'plain':
-//       return plain(obj1, obj2);
-//     case 'json':
-//       return json(obj1, obj2);
-//     default:
-//       return stylish(obj1, obj2);
-//   }
-// };
