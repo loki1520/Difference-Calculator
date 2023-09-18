5ddd76fd8ef310e8915825a10f4a6da55fcd9211
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-09-18 19:36:45 +0530

    Fix Step8

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,13 +3,16 @@ import plain from './plain.js';
 import json from './json.js';
 
 const resultConclusion = (obj1, obj2, format) => {
-  if (format === 'plain') {
-    return plain(obj1, obj2);
+  switch (format) {
+    case 'plain':
+      return plain(obj1, obj2);
+    case 'json':
+      return json(obj1, obj2);
+    case 'stylish':
+      return stylish(obj1, obj2);
+    default:
+      throw new Error('Attention! This file format is not supported!');
   }
-  if (format === 'json') {
-    return json(obj1, obj2);
-  }
-  return stylish(obj1, obj2);
 };
 
 export default resultConclusion;
