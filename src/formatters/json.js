c252413b39b49f39a1fc202dfbbcaaf7c0fd73c5
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-09-19 09:39:36 +0530

    Fix mistake

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,8 +1,8 @@
 import getTree from '../getTree.js';
 
-const getJsonViev = (obj1, obj2) => {
+const getJsonView = (obj1, obj2) => {
   const prepairedDiff = getTree(obj1, obj2);
   return JSON.stringify(prepairedDiff, null, ' ');
 };
 
-export default getJsonViev;
+export default getJsonView;
